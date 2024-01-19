63f12903fdde338395780abd9ec7d48887564572
kat <kat@pop-os.localdomain>
2024-01-19 12:51:47 +0000

    Configure Google Maps API.

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,24 @@
 import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
+import {
+  APIProvider,
+  Map,
+  AdvancedMarker,
+  Pin,
+  InfoWindow
+} from '@vis.gl/react-google-maps';
 
-function App() {
-  const [count, setCount] = useState(0);
+export default function App() {
+  const whipsnade = { lat: 51.85, lng: -0.54 };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <APIProvider apiKey={import.meta.env.VITE_GOOGLE_MAPS_API_KEY as string}>
+      <div style={{ height: "100vh" }}>
+        <Map
+          zoom={9}
+          center={whipsnade}
+          mapId={import.meta.env.VITE_GOOGLE_MAPS_MAP_ID as string}
+        />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </APIProvider>
   );
 }
-
-export default App;
