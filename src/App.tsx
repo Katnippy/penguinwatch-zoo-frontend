3176892892110dfb835f5a3350ac7a3484d4536e
kat <kat@pop-os.localdomain>
2024-01-25 18:22:13 +0000

    Create rudimentary header.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
+import Header from './components/Header';
 import GoogleMap from './components/GoogleMap';
 
 export default function App() {
   return (
-    <GoogleMap />
+    <>
+      <Header />
+      <GoogleMap />
+    </>
   );
 }
---
+++ src/components/Header.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <div className="header" style={{ textAlign: "center" }}>
+      <h1>Penguinwatch: Zoo</h1>
+    </div>
+  );
+}
