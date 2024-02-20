c1e941de51a27be0e6fe745fa5df700941f8d529
kat <kat@pop-os.localdomain>
2024-02-20 12:50:48 +0000

    Add button to toggle filter or add / update form.

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -8,7 +8,16 @@ import UpdateForm from './UpdateForm';
 type SidebarProps = ChangeZooProps;
 
 export default function Sidebar({ zoos, setZoos }: SidebarProps) {
+  const [filterOrAddUpdate, setFilterOrAddUpdate] = useState('filter');
   const [addOrUpdate, setAddOrUpdate] = useState('add');
+
+  function toggleFilterAddUpdate() {
+    if (filterOrAddUpdate === 'filter') {
+      setFilterOrAddUpdate('addupdate');
+    } else {
+      setFilterOrAddUpdate('filter');
+    }
+  }
 
   function toggleAddUpdate() {
     if (addOrUpdate === 'add') {
@@ -21,14 +30,25 @@ export default function Sidebar({ zoos, setZoos }: SidebarProps) {
   return (
     <div id="sidebar">
       <h1>Sidebar</h1>
-      <h3>Filter</h3>
-      <FilterForm zoos={zoos}/>
-      <h3>Add / Update</h3>
-      <button onClick={toggleAddUpdate}>
-        {addOrUpdate === 'add' ? 'Update' : 'Add'}
+      <button onClick={toggleFilterAddUpdate}>
+        {filterOrAddUpdate === 'addupdate' ? 'Filter' : 'Add / Update'}
       </button>
-      {addOrUpdate === 'add' ? <AddForm zoos={zoos} setZoos={setZoos}/>
-        : <UpdateForm zoos={zoos} setZoos={setZoos} />}
+      {filterOrAddUpdate === 'filter' && (
+        <>
+          <h3>Filter</h3>
+          <FilterForm zoos={zoos}/>
+        </>
+      )}
+      {filterOrAddUpdate === 'addupdate' && (
+        <>
+          <h3>Add / Update</h3>
+          <button onClick={toggleAddUpdate}>
+            {addOrUpdate === 'add' ? 'Update' : 'Add'}
+          </button>
+          {addOrUpdate === 'add' ? <AddForm zoos={zoos} setZoos={setZoos}/>
+            : <UpdateForm zoos={zoos} setZoos={setZoos} />}
+        </>
+      )}
     </div>
   );
 }
