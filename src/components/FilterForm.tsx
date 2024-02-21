c0e7031df83a3385ace5db765cfd292b832c6b07
kat <kat@pop-os.localdomain>
2024-02-21 14:12:24 +0000

    Enable filter zoo by location.

--- src/components/FilterForm.tsx
+++ src/components/FilterForm.tsx
@@ -11,15 +11,19 @@ type FilterFormProps = {
 
 export default function FilterForm({ zoos, setShownZoos }: FilterFormProps) {
   const [filteredName, setFilteredName] = useState('');
+  const [filteredLocation, setFilteredLocation] = useState('');
   // const [showWithinLocation, setShowWithinLocation] = useState(false);
   // const [showWithinCoords, setShowWithinCoords] = useState(false);
   const [checkedSpecies, setCheckedSpecies] = useState<Array<string>>([]);
 
+  // TODO: Refactor.
   function filterZoos(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault();
 
     setShownZoos(zoos.filter((zoo) =>
       zoo.name.toLowerCase().includes(filteredName.toLowerCase())));
+    setShownZoos((shownZoos) => shownZoos.filter((zoo) =>
+      zoo.location.toLowerCase().includes(filteredLocation.toLowerCase())));
     // ? Any way to make this more efficient with sets or something like that?
     // Set `shownZoos` to be every zoo already being shown that includes all of
     // (but not only) the checked species.
@@ -30,6 +34,10 @@ export default function FilterForm({ zoos, setShownZoos }: FilterFormProps) {
 
   function handleNameChange(event: React.ChangeEvent<HTMLInputElement>) {
     setFilteredName(event.target.value);
+  }
+
+  function handleLocationChange(event: React.ChangeEvent<HTMLInputElement>) {
+    setFilteredLocation(event.target.value);
   }
 
   function handleSpeciesChange(event: React.ChangeEvent<HTMLInputElement>) {
@@ -48,9 +56,10 @@ export default function FilterForm({ zoos, setShownZoos }: FilterFormProps) {
         <Input name={'name'} text={'Name: '} value={filteredName}
           onChange={handleNameChange} required={false} />
         <br />
-        {/* <label htmlFor="location">Location: </label>
-        <input id="location" />
-        <br /> */}
+        <Input name={'location'} text={'Location: '}
+          value={filteredLocation} onChange={handleLocationChange}
+          required={false} />
+        <br />
         {/* {showWithinLocation ? <Fieldset name={'location'} /> : '' } */}
         {/* <label htmlFor="lat">Coordinates: </label>
         <input id="lat" />
